b96ea3c1e205944ebc221df3d5faecc133d8748b
Daman Arora <61474540+Damans227@users.noreply.github.com>
2022-01-19 06:03:20 -0500

    Time: 42 ms (29.53%), Space: 14.1 MB (90.58%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -0,0 +1,6 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        res=" " #Initialize an empty string to store the result string
+        sList=s.split(" ")
+        truncatedList = sList[:k] #Truncate the list upto k cahracters via slicing
+        return res.join(truncatedList) #use join() method to convert list to a string
