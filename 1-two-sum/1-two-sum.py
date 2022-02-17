e58567d08ef828e1b043160db03df0e58c9453b5
Daman Arora <61474540+Damans227@users.noreply.github.com>
2022-02-17 06:14:42 -0500

    Time: 58 ms (89.84%), Space: 15.2 MB (59.01%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # nums = [2,7,11,15], target = 9
+        # O(n^2) Solution
+        # for i in range(0,len(nums)): # Use this for loop to select num1
+        #     num1=nums[i]
+        #     for j in range(i+1, len(nums)): # Use this for loop to select num2
+        #         complement=target-num1
+        #         if (nums[j]==complement):
+        #             return [i,j]
+        # return []
         
-        for i in range(0,len(nums)): # Use this for loop to select num1
-            num1=nums[i]
-            for j in range(i+1, len(nums)): # Use this for loop to select num2
-                complement=target-num1
-                if (nums[j]==complement):
-                    return [i,j]
-        return []
+        #O(n) Solution
+        hashMap={}
+        for i, n in enumerate(nums):
+            check=target-n
+            if check not in hashMap:
+                hashMap[n]=i
+            else:
+                return [hashMap[check],i]
+        return
