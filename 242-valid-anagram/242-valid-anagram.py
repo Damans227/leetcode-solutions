a74329da11f66a19f5c0caba81b05acf2ede5162
Daman Arora <61474540+Damans227@users.noreply.github.com>
2022-02-19 10:10:25 -0500

    Time: 60 ms (61.81%), Space: 14.4 MB (77.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,18 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        #brute force way
+        #brute force way O(s+t)
         sMap = {}
         tMap = {}
         
         
-        #O(n)
+        #O(s)
         for c in s:
             sMap[c] = 1 + sMap.get(c, 0)
-        #O(n)
+        #O(t)
         for c in t:
             tMap[c] = 1 + tMap.get(c, 0)
-        #O(n)
+
         for k in sMap.keys():
             if((k not in tMap) or (len(sMap)!=len(tMap)) or sMap[k]!=tMap[k]):
                 return False
