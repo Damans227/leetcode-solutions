c9c1143211429e2993d1b3253d1d0fd629fb2da5
Daman Arora <61474540+Damans227@users.noreply.github.com>
2022-03-16 19:35:14 -0400

    Time: 93 ms (29.92%), Space: 17.4 MB (96.89%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -7,10 +7,10 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow, fast = head, head
-        
+        # Optimized solution using Floyds tortoise and hare algorithm
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+            slow = slow.next # Address of the next node in Linked list 
+            fast = fast.next.next # Address of the next to next node in Linked list 
             if slow == fast:
                 return True
         
